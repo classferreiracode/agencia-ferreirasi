3efa2f3823a77df2b06680788a95735052f464dc
Wagner Ferreira <ferreirasi.tk@outlook.com>
2025-09-25 14:54:06 -0300

    update FormContact.vue

--- src/components/FormContact.vue
+++ src/components/FormContact.vue
@@ -15,7 +15,7 @@
         <FormTextarea v-model="form.message" label="Mensagem" />
         <span v-if="errors.message" class="error">{{ errors.message }}</span>
 
-        <div>
+        <div class="mt-4">
             <button
                 type="submit"
                 class="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-300"
